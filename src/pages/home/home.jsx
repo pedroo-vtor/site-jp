beb43b1d33d9faf806bf598bb61a1429f8a40ad1
Pedro Vitor <trpedrovitor21@gmail.com>
2025-09-29 10:57:14 -0300

    Atualizacao

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,32 +1,39 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link, } from 'react-router-dom';
 import Praias from '../praias/praias.jsx';
 import Zoologico from '../zoologico/zoologico.jsx';
 import Historia from '../historia/historia.jsx';
+import './home.css'
+import bandeira from '../../images/bandeira_jp.png'
 
 function Home() {
-  const [count, setCount] = useState(0)
     
   return (
     <>
-        <h1>Seja bem Vindo a João Pessoa!</h1>
-        <BrowserRouter>
-        {/* Navigation */}
-        <nav>
-        <Link className="link" to="/historia">Historia</Link>{" "}
-        <Link className="link" to="/praias">Praias</Link> {" "}
-        <Link className="link" to="/zoologico">Zoologico</Link> {" "}
-      </nav>
-
-      {/* Rotas */}
-      <Routes>
-        <Route path="/historia" element={<Historia />} />
-        <Route path="/praias" element={<Praias />} />
-        <Route path="/zoologico" element={<Zoologico />} />
-      </Routes>
-        </BrowserRouter>
+        <div className='main'>
+            <h1>Seja Bem Vindo a João Pessoa!</h1>
+            <div id="img_container">
+                <img src={bandeira} alt="Bandeira de João Pessoa" id="bandeira"/>
+            </div>
+           
+            <BrowserRouter>
+            {/* Navigation */}
+            <nav className="nav_links">
+            <Link className="link" to="/historia">Historia</Link>{" "}
+            <Link className="link" to="/praias">Praias</Link> {" "}
+            <Link className="link" to="/zoologico">Zoologico</Link> {" "}
+          </nav>
+    
+          {/* Rotas */}
+          <Routes>
+            <Route path="/historia" element={<Historia />} />
+            <Route path="/praias" element={<Praias />} />
+            <Route path="/zoologico" element={<Zoologico />} />
+          </Routes>
+            </BrowserRouter>
+        </div>
     </>
   )
 }
+
 
 export default Home
